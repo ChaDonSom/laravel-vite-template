011ef094854a50437b32dcfba4f7944c0f5f8377
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-01-26 09:45:25 -0500

    Add mdc

---
+++ resources/ts/core/buttons/Button.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="mdc-touch-target-wrapper" ref="mainRef">
+    <button class="mdc-button mdc-button--touch">
+      <span class="mdc-button__ripple"></span>
+      <span class="mdc-button__touch"></span>
+      <span class="mdc-button__focus-ring"></span>
+      <span class="mdc-button__label"><slot /></span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue'
+import { MDCRipple } from '@material/ripple'
+
+
+export default defineComponent({
+  setup() {
+    const mainRef = ref<HTMLElement | null>(null)
+    onMounted(() => {
+      let mdcButton: HTMLElement | null | undefined = mainRef.value?.querySelector('.mdc-button')
+      if (mdcButton) {
+        const buttonRipple = new MDCRipple(mdcButton)
+      }
+    })
+    
+    return {
+      mainRef,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@use "@/css/mdc-theme"; // modify styles to our specifications
+@use "@material/button/styles"; // apply all styles
+@use "@material/button"; // allow customizing, via @include button._____
+</style>
--- resources/ts/dev-intro/HelloWorld.vue
+++ resources/ts/dev-intro/HelloWorld.vue
@@ -1,33 +1,36 @@
 <template>
-	<h1>{{ msg }}</h1>
+	<div class="text-center m-3">
+		<h1>{{ msg }}</h1>
 
-	<p>
-		Recommended IDE setup:
-		<a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-		+
-		<a href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank">Vetur</a>
-		(or
-		<a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-		if using
-		<code>&ltscript setup&gt;</code>)
-	</p>
+		<p>
+			Recommended IDE setup:
+			<a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+			+
+			<a href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank">Vetur</a>
+			(or
+			<a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+			if using
+			<code>&ltscript setup&gt;</code>)
+		</p>
 
-	<p>See <code>README.md</code> for more information.</p>
+		<p>See <code>README.md</code> for more information.</p>
 
-	<p class="bg-gray-500">
-		<a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a> |
-		<a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-	</p>
+		<p class="bg-gray-500">
+			<a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a> |
+			<a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+		</p>
 
-	<button @click="count++">count is: {{ count }}</button>
-	<p>
-		Edit
-		<code>components/Testing.vue</code> to test hot module replacement.
-	</p>
+		<Button @click="count++">count is: {{ count }}</Button>
+		<p>
+			Edit
+			<code>components/Testing.vue</code> to test hot module replacement.
+		</p>
+	</div>
 </template>
 
 <script lang="ts">
 import { ref, defineComponent } from 'vue';
+import Button from '@/ts/core/buttons/Button.vue'
 export default defineComponent({
 	name: 'HelloWorld',
 	props: {
@@ -35,6 +38,9 @@ export default defineComponent({
 			type: String,
 			required: true,
 		},
+	},
+	components: {
+		Button,
 	},
 	setup: () => {
 		const count = ref(0);
