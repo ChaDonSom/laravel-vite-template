8b4b2b2eef960bca576a1ae59531e7d9aa0753b7
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-01-28 22:34:53 -0500

    Styling splash page

--- resources/ts/dev-intro/HelloWorld.vue
+++ resources/ts/dev-intro/HelloWorld.vue
@@ -5,19 +5,6 @@
 		</div>
 		<h1 class="text-3xl sm:text-5xl md:text-7xl font-thin">{{ msg }}</h1>
 
-		<p>
-			Recommended IDE setup:
-			<a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-			+
-			<a href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank">Vetur</a>
-			(or
-			<a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-			if using
-			<code>&ltscript setup&gt;</code>)
-		</p>
-
-		<p>See <code>README.md</code> for more information.</p>
-
 		<div class="flex justify-center m-2">
 			<p class="bg-gray-200 rounded-xl px-3 py-1">
 				<a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a> |
@@ -25,27 +12,38 @@
 			</p>
 		</div>
 
+		<h2 class="mt-5">Material Components:</h2>
 		<div class="flex justify-center gap-5">
 			<Button @click="count++" raised> Count is: {{ count }}</Button>
-			<Button @click="count++"> Count is: {{ count }}</Button>
 		</div>
-		<p>
-			Edit
-			<code>components/Testing.vue</code> to test hot module replacement.
-		</p>
-		<div class="flex justify-center m-4">
+		<h2 class="mt-5">Material Icons:</h2>
+		<div class="flex justify-center m-4 gap-5">
 			<i class="material-icons">edit</i>
+			<i class="material-icons">playlist_add_check</i>
+			<i class="material-icons">insert_emoticon</i>
+			<i class="material-icons">keyboard_hide</i>
 		</div>
-		<p>
-			Production: {{ prod }}<br>
-			BASE_URL: {{ baseUrl }}<br>
-			Small & larger breakpoint: {{ smAndLarger }}
-		</p>
+		<div class="my-7">
+			<h2 class="text-3xl sm:text-5xl md:text-6xl font-thin mb-3">Stack</h2>
+			<div class="flex flex-row flex-wrap align-center justify-center gap-5">
+				<a
+						v-for="tech of stack"
+						target="_blank"
+						class="flex flex-col items-center justify-center gap-1"
+						:key="tech.link"
+						:title="tech.title"
+						:href="tech.link"
+				>
+					<img :src="tech.image" style="height: 55px;" class="grow-0">
+					{{ tech.title }}
+				</a>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue';
+import { ref, defineComponent, reactive } from 'vue';
 import Button from '@/ts/core/buttons/Button.vue'
 import { breakpointsTailwind, useBreakpoints, useMediaQuery } from '@vueuse/core'
 import { vite_asset } from '@/ts/core/utilities/build'
@@ -63,17 +61,63 @@ export default defineComponent({
 	},
 	setup: () => {
 		const count = ref(0)
-		const isSmallScreen = useMediaQuery('(min-width: 640px)')
-		const isMediumScreen = useMediaQuery('(max-width: 768px)')
-		const isLargeScreen = useMediaQuery('(max-width: 1024px)')
 		const breakpoints = useBreakpoints(breakpointsTailwind)
 		const smAndLarger = breakpoints.greater('sm')
 		
 		const prod = import.meta.env.PROD
 		const baseUrl = import.meta.env.VITE_DEV_SERVER_URL
 
+		const stack = ref([
+			{
+				title: 'Laravel',
+				link: 'https://laravel.com',
+				image: 'https://laravel.com/img/logomark.min.svg'
+			},
+			{
+				title: 'Laravel Vite',
+				link: 'https://laravel-vite.innocenzi.dev',
+				image: 'https://laravel-vite.innocenzi.dev/logo.svg',
+			},
+			{
+				title: 'Vue Router',
+				link: 'https://next.router.vuejs.org/',
+				image: 'https://v3.vuejs.org/logo.png'
+			},
+			{
+				title: 'Vue 3',
+				link: 'https://v3.vuejs.org/',
+				image: 'https://v3.vuejs.org/logo.png'
+			},
+			{
+				title: 'Vite',
+				link: 'https://vitejs.dev',
+				image: 'https://vitejs.dev/logo.svg'
+			},
+			{
+				title: 'Pinia',
+				link: 'https://pinia.vuejs.org/',
+				image: 'https://pinia.vuejs.org/logo.svg'
+			},
+			{
+				title: 'Tailwind',
+				link: 'https://tailwindcss.com/',
+				image: 'https://pbs.twimg.com/profile_images/1468993891584073729/a_op8KnL_400x400.jpg'
+			},
+			{
+				title: 'Material Design Components',
+				link: 'https://github.com/material-components/material-components-web',
+				image: 'https://pbs.twimg.com/profile_images/925576484122779648/ucVTUoPg_400x400.jpg'
+			},
+			{
+				title: 'Vue Use',
+				link: 'https://vueuse.org/',
+				image: 'https://d33wubrfki0l68.cloudfront.net/2f6479d73bc25170dc532dd42e059166573bf478/61057/favicon.svg'
+			},
+		])
+
 		return {
 			count,
+			stack,
 			smAndLarger,
 			vite_asset,
 			prod,
