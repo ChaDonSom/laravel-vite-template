a7449fe765f59fe6f24efb5a8856418f7cab6895
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-02-10 09:41:16 -0500

    Add Save/Delete Buttons

---
+++ resources/ts/core/buttons/SaveButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <Button :disabled="disabled"
+  >
+    <template v-if="!loading">
+      Save
+    </template>
+    <template v-else>
+      <Circular small />
+    </template>
+    <template #trailing-icon>save</template>
+  </Button>
+</template>
+
+<script setup lang="ts">
+import Button from '@/ts/core/buttons/Button.vue';
+import Circular from '@/ts/core/loaders/Circular.vue'
+
+defineProps({
+  disabled: Boolean,
+  loading: Boolean,
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
