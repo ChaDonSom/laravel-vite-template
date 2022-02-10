dc597dad929a61a0b7dfbf314ccaefa6f553ffb4
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-02-10 09:42:40 -0500

    Add DollarsField for Dollars input

---
+++ resources/ts/core/fields/DollarsField.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="m-2 mt-4">
+    <label class="mdc-text-field mdc-text-field--outlined" ref="mainRef">
+      <span class="mdc-notched-outline">
+        <span class="mdc-notched-outline__leading"></span>
+        <span class="mdc-notched-outline__notch">
+          <span class="mdc-floating-label" :id="`textfield-label-${id}`"><slot /></span>
+        </span>
+        <span class="mdc-notched-outline__trailing"></span>
+      </span>
+      <span class="mdc-text-field__affix mdc-text-field__affix--prefix">$</span>
+      <input
+          :id="`textfield-input-${id}`"
+          type="number"
+          step="0.01"
+          min="0"
+          max="1000000000"
+          class="mdc-text-field__input"
+          :aria-labelledby="`textfield-label-${id}`"
+          :value="modelValue"
+          :autofocus="autofocus"
+          @focus="autoselect ? ($event.target as HTMLInputElement).select() : null"
+          @input="$emit('update:modelValue', ($event?.target as HTMLInputElement)?.value)"
+      >
+    </label>
+    <div class="mdc-text-field-helper-line max-w-fit">
+      <div 
+          id="my-helper-id" 
+          class="mdc-text-field-helper-text"
+          aria-hidden="true"
+          :style="{ opacity: 1, color: error ? 'rgb(181 30 30)' : undefined }"
+      >
+        {{ error ? error : (helper ? helper : '') }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MDCTextField } from '@material/textfield'
+import { onMounted, ref } from 'vue'
+
+const props = defineProps({
+  modelValue: [String, Number],
+  error: String,
+  helper: String,
+  autoselect: Boolean,
+  autofocus: Boolean,
+})
+
+const id = ref(Math.floor(Math.random() * 10000000))
+const mainRef = ref<Element | null>(null)
+const mdcTextfield = ref<MDCTextField | null>(null)
+onMounted(() => {
+  if (mainRef.value) mdcTextfield.value = new MDCTextField(mainRef.value)
+})
+</script>
+
+<style scoped lang="scss">
+@use "@/css/mdc-theme";
+@use "@material/floating-label/mdc-floating-label";
+@use "@material/line-ripple/mdc-line-ripple";
+@use "@material/notched-outline/mdc-notched-outline";
+@use "@material/textfield";
+
+@include textfield.core-styles;
+
+.mdc-text-field {
+  @include textfield.outline-shape-radius(mdc-theme.$textfield-shape-radius);
+  // @include textfield.outlined-density(-4);
+}
+
+// Tailwind 'undo' RE MDC mess-up, recommended by Adam Wathan (fixes tons of borders showing up on focus in MDC)
+*,
+*::before,
+*::after {
+  border-width: 1px;
+  border-style: none;
+}
+
+input[type="date"]::-webkit-calendar-picker-indicator {
+    display: block;
+    -webkit-appearance: button;
+}
+</style>
