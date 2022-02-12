9a3b840ae8021104877ce7b6187147b420680e6b
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-02-12 11:06:22 -0500

    Add icon button

---
+++ resources/ts/core/buttons/IconButton.vue
@@ -0,0 +1,26 @@
+<template>
+<button class="mdc-icon-button material-icons" ref="mainRef">
+  <div class="mdc-icon-button__ripple"></div>
+  <span class="mdc-icon-button__focus-ring"></span>
+  <slot />
+</button>
+</template>
+
+<script setup lang="ts">
+import { MDCRipple } from '@material/ripple';
+import { onMounted, ref } from 'vue';
+
+const mainRef = ref(null)
+const iconButtonRipple = ref<MDCRipple|null>(null)
+onMounted(() => {
+  if (mainRef.value) {
+    iconButtonRipple.value = new MDCRipple(mainRef.value);
+    iconButtonRipple.value.unbounded = true;
+  }
+})
+</script>
+
+<style scoped lang="scss">
+@use "@/css/mdc-theme";
+@use "@material/icon-button/styles";
+</style>
