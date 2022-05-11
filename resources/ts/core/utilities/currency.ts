9090f26e97cd63af60c87edcd8dd8e225e47fea9
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-05-11 11:02:56 -0400

    Add commas to dollars fn

--- resources/ts/core/utilities/currency.ts
+++ resources/ts/core/utilities/currency.ts
@@ -1,4 +1,16 @@
-export const dollars = (value: number): string => `$${rounded(value)}`
+export const dollars = (value: number): string => {
+  let split = `$${rounded(value)}`.split('')
+  let result = []
+  let decimalIndex = split.indexOf('.') - 1
+  for (let i =  0; i < split.length; i++) {
+    result.push(split[i])
+    if (i < decimalIndex) {
+      if ((decimalIndex - i) % 3 == 0 && i != 0) result.push(',')
+    }
+  }
+  return result.join('')
+}
+
 export const rounded = (value: number, decimals?: number): string => {
   let rounded = Math.round(value * 100) / 100
   if (!decimals && typeof decimals !== 'number') return rounded.toFixed(2)
