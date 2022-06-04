e50f143864752bfed3f2901f96fb2985ddd9c0c9
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-06-03 22:12:33 -0400

    Snackbar component

---
+++ resources/ts/core/snackbars/SnackbarDismissButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <IconButton class="mdc-snackbar__dismiss" primary>
+    <slot></slot>
+  </IconButton>
+</template>
+
+<script lang="ts" setup>
+import Button from '@/ts/core/buttons/Button.vue';
+import IconButton from '@/ts/core/buttons/IconButton.vue';
+</script>
+
+<style lang="scss" scoped>
+</style>
