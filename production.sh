1b82ad12be48a65c55cd920dcdb26109800355b8
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-06-09 08:10:21 -0400

    Fix svg/png if in prod script

--- production.sh
+++ production.sh
@@ -6,11 +6,11 @@ cp public/build/manifest.webmanifest public
 cp public/build/manifest.webmanifest public/manifest.json
 
 # All static assets must be listed here to show up
-if [ -f public/build/*.png]
+if [ -f public/build/*.png ]; then
     cp public/build/*.png public
 fi
 
-if [ -f public/build/*.svg]
+if [ -f public/build/*.svg ]; then 
     cp public/build/*.svg public
 fi
 # cp public/build/*.ico public
