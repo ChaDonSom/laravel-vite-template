371805213819fd50b446b11dc819aeda7aea6d5f
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-09-06 15:43:00 -0400

    Reset / path for php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return response()->file('../dist/index.html');
+    return view('app');
 })->name('app');
 
 // TODO:
