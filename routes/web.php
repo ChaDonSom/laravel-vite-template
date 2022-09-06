83e5c2cc1efdbe38e1874c9622bde7c1d995237e
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-09-06 15:07:21 -0400

    Try again

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return response()->file('/dist/index.html');
+    return response()->file('./dist/index.html');
 })->name('app');
 
 // TODO:
