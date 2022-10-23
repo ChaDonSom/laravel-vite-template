b8998ad823b61ebd0a2765c7cf7e68fcbbaa4564
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-10-23 13:40:44 -0400

    Update sanctum domains to include app_url & vite_dev_server_url

--- config/sanctum.php
+++ config/sanctum.php
@@ -14,9 +14,12 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,localhost:8000,127.0.0.1,127.0.0.1:8000,::1',
-        env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
+        '%s%s%s%s%s',
+        'localhost,localhost:3000,localhost:8000,127.0.0.1,127.0.0.1:8000,127.0.0.1:3000,::1',
+        env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
+        env('VITE_DEV_SERVER_URL') ? ',' . parse_url(env('VITE_DEV_SERVER_URL'), PHP_URL_HOST) : '',
+        env('APP_URL') ? ',' . explode('://', env('APP_URL'))[1] : '',
+        env('VITE_DEV_SERVER_URL') ? ',' . explode('://', env('VITE_DEV_SERVER_URL'))[1] : '',
     ))),
 
     /*
