5c3331f1935a12ab2e2b8f2d4e347aae16c4b6f1
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-10-25 22:25:24 -0400

    Revert "Add start / game routes"
    This reverts commit c33bffdf63fe0767336d11e5cf0b4e0cf1184ef8.

--- src/experiments/phaser-tutorial/index.ts
+++
@@ -1,3 +0,0 @@
-import { ref } from "vue";
-
-export const playerCount = ref<number>(1)
--- src/router/experiments.ts
+++ src/router/experiments.ts
@@ -1,20 +1,8 @@
 import type { RouteRecordRaw } from "vue-router";
-const GameVue = () => import("@/experiments/phaser-tutorial/Game.vue")
-const StartVue = () => import("@/experiments/phaser-tutorial/Start.vue")
 const PhaserTutorial = () => import("@/experiments/phaser-tutorial/PhaserTutorial.vue")
 
 const routes: RouteRecordRaw[] = [
-    {
-        name: "phaser-tutorial", path: "/phaser-tutorial", component: PhaserTutorial, props: true,
-        children: [
-            {
-                name: "phaser-tutorial-game", path: "game", component: GameVue
-            },
-            {
-                name: "phaser-tutorial-start", path: "start", component: StartVue
-            }
-        ]
-    },
+    { name: "phaser-tutorial", path: "/phaser-tutorial", component: PhaserTutorial, props: true },
 ];
 
 export default routes;
