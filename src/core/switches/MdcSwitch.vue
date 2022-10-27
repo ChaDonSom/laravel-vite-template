20b6cb4c0f3cac8ede8cd2f96c6c2f0789ac9366
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-10-27 13:30:59 -0400

    Add MDC Switch

---
+++ src/core/switches/MdcSwitch.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="m-2 inline-flex gap-3">
+    <button
+        :id="`basic-switch-${randomId}`"
+        class="mdc-switch"
+        :class="{
+          'mdc-switch--unselected': !modelValue,
+          'mdc-switch--selected': modelValue
+        }"
+        type="button"
+        role="switch"
+        :aria-checked="modelValue"
+        ref="mdcSwitchEl"
+    >
+      <div class="mdc-switch__track"></div>
+      <div class="mdc-switch__handle-track">
+        <div class="mdc-switch__handle">
+          <div class="mdc-switch__shadow">
+            <div class="mdc-elevation-overlay"></div>
+          </div>
+          <div class="mdc-switch__ripple"></div>
+          <div class="mdc-switch__icons">
+            <svg class="mdc-switch__icon mdc-switch__icon--on" viewBox="0 0 24 24">
+              <path d="M19.69,5.23L8.96,15.96l-4.23-4.23L2.96,13.5l6,6L21.46,7L19.69,5.23z" />
+            </svg>
+            <svg class="mdc-switch__icon mdc-switch__icon--off" viewBox="0 0 24 24">
+              <path d="M20 13H4v-2h16v2z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <span class="mdc-switch__focus-ring-wrapper">
+        <div class="mdc-switch__focus-ring"></div>
+      </span>
+    </button>
+    <label :for="`basic-switch-${randomId}`">
+      <slot v-if="$slots.default"></slot>
+      <span v-if="!$slots.default && label">{{ label }}</span>
+    </label>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { MDCSwitch } from '@material/switch'
+import { ref, watch } from 'vue'
+
+defineProps({
+  modelValue: {
+    type: Boolean,
+    default: () => false,
+  },
+  label: {
+    type: String,
+  }
+})
+
+const randomId = ref(Math.round(Math.random() * 100000).toFixed(0))
+const mdcSwitchEl = ref<HTMLButtonElement|null>(null)
+const mdcSwitch = ref<MDCSwitch|null>(null)
+watch(
+  () => mdcSwitchEl.value,
+  () => {
+    if (mdcSwitchEl.value) {
+      mdcSwitch.value = new MDCSwitch(mdcSwitchEl.value)
+    }
+  }
+)
+</script>
+
+<style lang="scss" scoped>
+@use "@/css/mdc-theme";
+@use '@material/switch/styles';
+</style>
