f238c8f5b6017876f0f2fb4c31dd60f32bf049d8
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-11-08 13:39:45 -0500

    OutlinedTextarea for large text

---
+++ src/core/fields/OutlinedTextarea.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="m-2 mt-4">
+        <label class="mdc-text-field mdc-text-field--outlined mdc-text-field--textarea" ref="mainRef">
+            <!-- mdc-text-field--no-label -->
+            <span class="mdc-notched-outline">
+                <span class="mdc-notched-outline__leading"></span>
+                <span class="mdc-notched-outline__notch">
+                    <span
+                        class="mdc-floating-label"
+                        :id="`textarea-label-${id}`"
+                        ><slot
+                    /></span>
+                </span>
+                <span class="mdc-notched-outline__trailing"></span>
+            </span>
+            <span class="mdc-text-field__resizer">
+                <textarea
+                    :id="`textarea-input-${id}`"
+                    class="mdc-text-field__input"
+                    rows="4"
+                    cols="30"
+                    :aria-labelledby="`textarea-label-${id}`"
+                    :value="modelValue"
+                    :autofocus="autofocus"
+                    v-bind="bindableAttributes"
+                    @focus="
+                        autoselect
+                            ? ($event.target as HTMLInputElement).select()
+                            : null
+                    "
+                    @input="
+                        $emit(
+                            'update:modelValue',
+                            ($event?.target as HTMLInputElement)?.value
+                        )
+                    "
+                ></textarea>
+                    <!-- aria-label="Label" -->
+            </span>
+        </label>
+        <div class="mdc-text-field-helper-line max-w-fit">
+            <div
+                id="my-helper-id"
+                class="mdc-text-field-helper-text"
+                aria-hidden="true"
+                :style="{
+                    opacity: 1,
+                    color: error ? 'rgb(181 30 30)' : undefined,
+                }"
+            >
+                {{ error ? error : helper ? helper : "" }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { MDCTextField } from "@material/textfield";
+import { computed, onMounted, ref } from "vue";
+
+const props = defineProps({
+    modelValue: [String, Number],
+    type: String,
+    error: String,
+    helper: String,
+    autoselect: Boolean,
+    autofocus: Boolean,
+})
+
+const bindableAttributes = computed(() => {
+    let result: { [key: string]: any } = {}
+    return result
+});
+
+const id = ref(Math.floor(Math.random() * 10000000));
+const mainRef = ref<Element | null>(null);
+const mdcTextfield = ref<MDCTextField | null>(null);
+onMounted(() => {
+    if (mainRef.value) mdcTextfield.value = new MDCTextField(mainRef.value);
+    if (props.autofocus) mainRef.value?.querySelector("textarea")?.focus();
+});
+</script>
+
+<style scoped lang="scss">
+@use "@/css/mdc-theme";
+@use "@material/floating-label/mdc-floating-label";
+@use "@material/line-ripple/mdc-line-ripple";
+@use "@material/notched-outline/mdc-notched-outline";
+@use "@material/textfield";
+
+@include textfield.core-styles;
+
+.mdc-text-field {
+    @include textfield.outline-shape-radius(mdc-theme.$textfield-shape-radius);
+    @if (mdc-theme.$textfield-shape-radius > 4px) {
+        textarea.mdc-text-field__input {
+            padding-inline: 0;
+        }
+    }
+    // @include textfield.outlined-density(-4);
+    @include textfield.outline-color(var(--color-border));
+    @include textfield.label-color(var(--color-text));
+    @include textfield.ink-color(var(--color-text));
+    @include textfield.hover-outline-color(var(--color-border-hover));
+}
+
+// Tailwind 'undo' RE MDC mess-up, recommended by Adam Wathan (fixes tons of borders showing up on focus in MDC)
+*,
+*::before,
+*::after {
+    border-width: 1px;
+    border-style: none;
+}
+
+// For some reason, if we don't have this, date-type inputs don't show the calendar icon
+input[type="date"]::-webkit-calendar-picker-indicator {
+    display: block;
+    -webkit-appearance: button;
+}
+</style>
